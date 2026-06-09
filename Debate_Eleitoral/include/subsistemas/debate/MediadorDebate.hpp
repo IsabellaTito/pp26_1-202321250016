87346a26c1fffac6ba24466802e873c93354ef31
Isabella Tito <isabellatito76@gmail.com>
2026-06-09 20:59:00 -0300

    Acrescimo da lógica para Direito de Resposta

--- Debate_Eleitoral/include/subsistemas/debate/MediadorDebate.hpp
+++ Debate_Eleitoral/include/subsistemas/debate/MediadorDebate.hpp
@@ -1,22 +1,29 @@
 #pragma once
 
 #include <memory>
+#include <queue>
 
 #include "subsistemas/debate/MediadorBase.hpp"
 #include "subsistemas/debate/Inquiridor.hpp"
 #include "subsistemas/debate/Inquirido.hpp"
+#include "subsistemas/estados/EstadoCiclos.hpp"
+#include "subsistemas/estados/DireitoResposta.hpp"
+#include "subsistemas/GerenciaPolitico.hpp"
 
-using std::shared_ptr;
+using std::shared_ptr, std::queue, std::make_shared;
 
 class ColaboradorPolitico;
 
-class MediadorDebate : public MediadorBase{
+class MediadorDebate : public MediadorBase, public std::enable_shared_from_this<MediadorDebate> {
     private:
         shared_ptr<Inquiridor> inquiridor;
-
         shared_ptr<Inquirido> inquirido;
+        shared_ptr<EstadosDebate> estado;
+        queue<shared_ptr<ColaboradorPolitico>> listaDR;
 
     public:
+
+        MediadorDebate();
 
         void setInquiridor(shared_ptr<ColaboradorPolitico> politico);
 
@@ -27,5 +34,13 @@ class MediadorDebate : public MediadorBase{
         shared_ptr<Inquirido> getInquirido() const;
 
         void debate(const ConfiguraTempo& config) override;
+
+        void addListaDR(shared_ptr<ColaboradorPolitico> politico){listaDR.push(politico);}
+
+        queue<shared_ptr<ColaboradorPolitico>>& getListaDR(){return listaDR;}
+
+        void removeElementoDR(){listaDR.pop();}
+
+        void setEstado(shared_ptr<EstadosDebate> estado){this->estado = estado;}
 
 };
